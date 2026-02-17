b66d897c290e78c5fb192cf230cab42d1cdd262c
varun <varun.s.patil@wdtablesystems.com>
2026-02-17 23:36:02 +0530

    gpio_init function: Ignoring the Interrupt part just coding the moder part for init.

--- STM32F407_GPIO_Driver/Custom_Drivers/Src/gpio.c
+++ STM32F407_GPIO_Driver/Custom_Drivers/Src/gpio.c
@@ -57,3 +57,11 @@ void gpio_clk_ctrl(GPIO_TypeDef *pGPIOx, uint8_t state)
         }        
     }
 }
+
+void gpio_init(GPIO_HandleTypeDef *pGPIO_Handle)
+{
+    // 1. Configure Mode of the pin, o/p or input & other stuff.
+
+    uint8_t pin_number = pGPIO_Handle->Pin_Settings.Pin;
+    pGPIO_Handle->pGPIOx->MODER = pGPIO_Handle->Pin_Settings.Mode << (pin_number)*2;
+}
