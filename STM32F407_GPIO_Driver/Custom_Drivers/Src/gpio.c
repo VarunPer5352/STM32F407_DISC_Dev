a494759d49a05d2acb61a3f5ccf4653788a3327e
varun <varun.s.patil@wdtablesystems.com>
2026-02-17 23:09:36 +0530

    ADD: Function to enable or disable peripheral clock to a GPIO port.

--- STM32F407_GPIO_Driver/Custom_Drivers/Src/gpio.c
+++ STM32F407_GPIO_Driver/Custom_Drivers/Src/gpio.c
@@ -7,4 +7,53 @@
 
 #include "gpio.h"
 
-
+void gpio_clk_ctrl(GPIO_TypeDef *pGPIOx, uint8_t state)
+{
+    if (state == ENABLE)
+    {
+        if(pGPIOx == GPIOA)
+        {
+            GPIOA_PCLK_EN();
+        }
+        else if(pGPIOx == GPIOB)
+        {
+            GPIOB_PCLK_EN();
+        }
+        else if(pGPIOx == GPIOC)
+        {
+            GPIOC_PCLK_EN();
+        }
+        else if(pGPIOx == GPIOD)
+        {
+            GPIOD_PCLK_EN();
+        }
+        else if(pGPIOx == GPIOE)
+        {
+            GPIOE_PCLK_EN();
+        }
+    }
+    else
+    {
+        // Diabling clock to a gpio port:
+        if(pGPIOx == GPIOA)
+        {
+            GPIOA_PCLK_DI();
+        }
+        else if(pGPIOx == GPIOB)
+        {
+            GPIOB_PCLK_DI();
+        }
+        else if(pGPIOx == GPIOC)
+        {
+            GPIOC_PCLK_DI();
+        }
+        else if(pGPIOx == GPIOD)
+        {
+            GPIOD_PCLK_DI();
+        }
+        else if(pGPIOx == GPIOE)
+        {
+            GPIOE_PCLK_DI();
+        }        
+    }
+}
