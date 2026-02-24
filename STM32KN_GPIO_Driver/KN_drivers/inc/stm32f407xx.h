8acc3c4eb237344d586a3f6b851a8ebe4289ee00
varun <varun.s.patil@wdtablesystems.com>
2026-02-25 00:26:55 +0530

    VVI: Renamed the copied header file from template stm32f407xx.h to hal_.... as its hal based for distinction between that & the KN;'s course header file stm32f407xx.h from now on.

---
+++ STM32KN_GPIO_Driver/KN_drivers/inc/stm32f407xx.h
@@ -0,0 +1,13 @@
+/*
+ * stm32f407xx.h
+ *
+ *  Created on: Feb 25, 2026
+ *      Author: varun.s.patil
+ */
+
+#ifndef INC_STM32F407XX_H_
+#define INC_STM32F407XX_H_
+
+
+
+#endif /* INC_STM32F407XX_H_ */
