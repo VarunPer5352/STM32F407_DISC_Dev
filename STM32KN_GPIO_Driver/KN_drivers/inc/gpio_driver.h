316130ff0a7a20abef6ad288cdbf5c99f2b5b772
varun <varun.s.patil@wdtablesystems.com>
2026-03-06 01:12:50 +0530

    Added driver .h & .c files.

---
+++ STM32KN_GPIO_Driver/KN_drivers/inc/gpio_driver.h
@@ -0,0 +1,13 @@
+/*
+ * gpio_driver.h
+ *
+ *  Created on: Mar 6, 2026
+ *      Author: varun.s.patil
+ */
+
+#ifndef INC_GPIO_DRIVER_H_
+#define INC_GPIO_DRIVER_H_
+
+
+
+#endif /* INC_GPIO_DRIVER_H_ */
---
+++ STM32KN_GPIO_Driver/KN_drivers/src/gpio_driver.c
@@ -0,0 +1,8 @@
+/*
+ * gpio_driver.c
+ *
+ *  Created on: Mar 6, 2026
+ *      Author: varun.s.patil
+ */
+
+
