9b2898f980fc66340e67047af1993ca154368c27
varun <varun.s.patil@wdtablesystems.com>
2026-03-06 00:06:35 +0530

    Added GPIO register structure decl'n in .h file.

--- STM32KN_GPIO_Driver/KN_drivers/inc/stm32f407xx.h
+++ STM32KN_GPIO_Driver/KN_drivers/inc/stm32f407xx.h
@@ -279,4 +279,20 @@ typedef enum
 #define FLASHSIZE_BASE               0x1FFF7A22UL           /*!< FLASH Size register base address       */
 #define PACKAGE_BASE                 0x1FFF7BF0UL           /*!< Package size register base address     */
 
+/************************************************************************/
+/* Structuring of Peripheral Register's */
+/************************************************************************/
+typedef struct 
+{
+    __IO uint32_t MODER;    /*!< GPIO port mode register,               Address offset: 0x00      */
+    __IO uint32_t OTYPER;   /*!< GPIO port output type register,        Address offset: 0x04      */
+    __IO uint32_t OSPEEDR;  /*!< GPIO port output speed register,       Address offset: 0x08      */
+    __IO uint32_t PUPDR;    /*!< GPIO port pull-up/pull-down register,  Address offset: 0x0C      */
+    __IO uint32_t IDR;      /*!< GPIO port input data register,         Address offset: 0x10      */
+    __IO uint32_t ODR;      /*!< GPIO port output data register,        Address offset: 0x14      */
+    __IO uint32_t BSRR;     /*!< GPIO port bit set/reset register,      Address offset: 0x18      */
+    __IO uint32_t LCKR;     /*!< GPIO port configuration lock register, Address offset: 0x1C      */
+    __IO uint32_t AFR[2];   /*!< GPIO alternate function registers,     Address offset: 0x20-0x24 */
+}GPIO_RegDef_t;
+
 #endif /* INC_STM32F407XX_H_ */
