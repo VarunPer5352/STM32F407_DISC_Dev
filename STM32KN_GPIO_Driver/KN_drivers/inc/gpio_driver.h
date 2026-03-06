c928a902201250c347df7577f66b04fdde3056cc
varun <varun.s.patil@wdtablesystems.com>
2026-03-06 20:54:36 +0530

    Added the handle structure & Config structure for GPIO specific header.

--- STM32KN_GPIO_Driver/KN_drivers/inc/gpio_driver.h
+++ STM32KN_GPIO_Driver/KN_drivers/inc/gpio_driver.h
@@ -8,6 +8,26 @@
 #ifndef INC_GPIO_DRIVER_H_
 #define INC_GPIO_DRIVER_H_
 
+#include "stm32f407xx.h"
 
+/************************************************************************/
+/* A Configuration Structure for GPIO pin holding all its settings */
+/************************************************************************/
+typedef struct{
+    uint32_t Pin;       /* Specifies the GPIO pin number to be configured.*/
+    uint32_t Mode;      /* Specifies the operating mode for the selected pins.*/
+    uint32_t Pull;      /* Specifies the Pull-up or Pull-Down activation for the selected pins.*/
+    uint32_t OPType;    /* Specifies the OutPut type settings Open drain, push pull etc.*/
+    uint32_t Speed;     /* Specifies the speed for the selected pins.*/
+    uint32_t Alternate; /* Peripheral to be connected to the selected pins.*/
+}GPIO_Config_t;
+
+/************************************************************************/
+/* A Handle Structure for GPIO pin */
+/************************************************************************/
+typedef struct{
+    GPIO_RegDef_t *pGPIOx_addr; // Pointer to hold the addresss of a GPIO{A-I} periph which has the pin.
+    GPIO_Config_t pin_config; // This has all the settings attributed to that GPIO, this is used to store settings to be written into Reg's
+}GPIO_Handle_t;
 
 #endif /* INC_GPIO_DRIVER_H_ */
