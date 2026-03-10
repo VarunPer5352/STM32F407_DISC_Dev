87a812620f88dfd2c82af962e38f0b254dd2cef7
varun <varun.s.patil@wdtablesystems.com>
2026-03-10 23:55:34 +0530

    Creating SPI .c & .h files.

---
+++ STM32KN_SPI_Driver/KN_drivers/inc/spi_driver.h
@@ -0,0 +1,13 @@
+/*
+ * spi_driver.h
+ *
+ *  Created on: Mar 10, 2026
+ *      Author: varun.s.patil
+ */
+
+#ifndef INC_SPI_DRIVER_H_
+#define INC_SPI_DRIVER_H_
+
+
+
+#endif /* INC_SPI_DRIVER_H_ */
---
+++ STM32KN_SPI_Driver/KN_drivers/src/spi_driver.c
@@ -0,0 +1,8 @@
+/*
+ * spi_driver.c
+ *
+ *  Created on: Mar 10, 2026
+ *      Author: varun.s.patil
+ */
+
+
