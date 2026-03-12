8b0d04d36e5313153fee5f568b7bb9653a845923
varun <varun.s.patil@wdtablesystems.com>
2026-03-12 22:28:40 +0530

    Implementing spi_clk_ctrl function.

--- STM32KN_SPI_Driver/KN_drivers/src/spi_driver.c
+++ STM32KN_SPI_Driver/KN_drivers/src/spi_driver.c
@@ -5,4 +5,85 @@
  *      Author: varun.s.patil
  */
 
+#include "spi_driver.h"
 
+/***************************************************************************
+ * @brief       Controls the peripheral clock for the specified SPI
+ *
+ * @details     This function enables or disables the clock for a given
+ *              SPI peripheral by configuring the corresponding RCC
+ *              peripheral clock enable register.
+ *
+ *              In STM32F407, SPI peripherals are connected to two
+ *              different APB buses:
+ *
+ *                  SPI1 → APB2 bus
+ *                  SPI2 → APB1 bus
+ *                  SPI3 → APB1 bus
+ *                  SPI4 → APB2 bus
+ *
+ *              The clock must be enabled before accessing any SPI
+ *              registers. If the clock is disabled, register writes
+ *              will have no effect.
+ *
+ * @param[in]   pSPIx_addr
+ *              Pointer to the SPI peripheral base address.
+ *              Possible values:
+ *                  SPI1
+ *                  SPI2
+ *                  SPI3
+ *                  SPI4
+ *
+ * @param[in]   state
+ *              Enable or disable control for the SPI clock.
+ *
+ *              Possible values:
+ *                  ENABLE
+ *                  DISABLE
+ *
+ * @return      None
+ *
+ * @note        Enabling the SPI clock is typically the first step
+ *              before configuring the SPI peripheral registers.
+ ***************************************************************************/
+void spi_clk_ctrl(SPI_RegDef_t *pSPIx_addr, uint8_t state)
+{
+    if (state == ENABLE)
+    {
+        if(pSPIx_addr == SPI1)
+        {
+            SPI1_PCLK_EN();
+        }
+        else if (pSPIx_addr == SPI2)
+        {
+            SPI2_PCLK_EN();
+        }
+        else if (pSPIx_addr == SPI3)
+        {
+            SPI3_PCLK_EN();
+        }
+        else if (pSPIx_addr == SPI4)
+        {
+            SPI4_PCLK_EN();
+        }
+    }
+    else if (state == DISABLE)
+    {
+        if(pSPIx_addr == SPI1)
+        {
+            SPI1_PCLK_DI();
+        }
+        else if (pSPIx_addr == SPI2)
+        {
+            SPI2_PCLK_DI();
+        }
+        else if (pSPIx_addr == SPI3)
+        {
+            SPI3_PCLK_DI();
+        }
+        else if (pSPIx_addr == SPI4)
+        {
+            SPI4_PCLK_DI();
+        }
+    }
+}
